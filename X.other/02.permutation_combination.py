f8a04f2dc46cfb06ed9e5840c0bdef855505cba3
phanyoung <phanyoung@pypot.com>
2014-11-25 22:24:50 +0800

    add comment

--- X.other/02.permutation_combination.py
+++ X.other/02.permutation_combination.py
@@ -1,26 +1,17 @@
 #_*_ coding: utf-8 _*_
 
-gOutBuf = []
-
-def Output(a):
-    gOutBuf.append(a)
-    
-def CleanOut():
-    global gOutBuf
-    gOutBuf = []
-    
-def ShowOut():
-    print '\n'.join(gOutBuf)
-    
+import sys
+sys.path.append('..')
+import common_debug    
 
 
-"""
+""" 字符串的全排列
 题目：输入一个字符串，打印出该字符串中字符的所有排列。例如输入字符串abc，则输出由字符a、b、c所能排列出来的所有字符串abc、acb、bac、bca、cab和cba。
 思路：递归，从未被选择的集合里选一个，放在指定的位置上
 """
 def PermutationCore(s, l, pos):
     if not s:
-        Output(''.join(l))
+        print ''.join(l)
         return
     for i, a in enumerate(s):
         l[pos] = a
@@ -31,28 +22,33 @@ def StrPermutation(s):
 
 
 
+
+""" 字符串的组合
+题目：输入一个字符串，打印出该字符串中字符的所有组合。例如输入字符串abc，则输出由字符a、b、c全部或者部分组合a/b/c/ab/ac/abc。
+思路：递归，和排列非常类似，主要区别是：
+1. 输出字符长度不固定
+2. 不能重复计算相同元素的组合，为了不重复，取字符时，要保证取出来的组合是输入字符的子序列
+3. 剩余元素不能恰好填满输出数组
+"""
 def CombinationCore(s, l, pos):
-    if len(s) < len(l)-pos:
+    if len(s) < len(l)-pos: #区别3
         return
-    if pos >= len(l) or not s:
+    if pos >= len(l): 
         print ''.join(l)
         return
     for i,a in enumerate(s):
         l[pos] = a
-        CombinationCore(s[i+1:], l, pos+1)
+        CombinationCore(s[i+1:], l, pos+1) #区别2
 
 
 def StrCombination(s):
-    s = sorted(s)
-    for n in xrange(1, 1 + len(s)):
+    for n in xrange(1, 1 + len(s)): #区别1
         l = [None] * n
         CombinationCore(s, l, 0)
 
 #test
 if __name__ == "__main__":
-    StrPermutation("abcde")
-    print len(gOutBuf)
-    #ShowOut()
-    CleanOut()
-    StrCombination("abcd")
-    ShowOut()
+    print "Permutation:"
+    StrPermutation("abcd")
+    print "\n\nCombination:"
+    StrCombination("bdac")
